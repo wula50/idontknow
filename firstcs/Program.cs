0478f030d7330e9f7e11902c16ea1660397b4d7b
Ada <wula50@163.com>
2020-04-05 00:07:25 +0800

    py and cs
    尝试在vscode里面调试py和cs

---
+++ firstcs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstcs
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!ada");
+        }
+    }
+}
---
+++ firstcs/hello.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+
+namespace Test {
+    class Program {
+        static void Main (string[] args) {
+            Console.Write (">>");
+            for (int i = 0; i < 100; i++) {
+                Console.WriteLine (i);
+            }
+            Console.Write ("\n>>");
+        }
+    }
+}
