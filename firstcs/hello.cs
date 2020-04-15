0322bcb7ce65bd067d83632863fced2f64728b8c
Ada <wula50@163.com>
2020-04-15 22:39:36 +0800

    uml

--- firstcs/hello.cs
+++ firstcs/hello.cs
@@ -8,7 +8,21 @@ namespace Test {
             for (int i = 0; i < 100; i++) {
                 Console.WriteLine (i);
             }
+            Student student = new Student (1, 2);
             Console.Write ("\n>>Cs中文一切正常，但是速度明显慢");
+            Console.WriteLine(student.Print());
+        }
+    }
+    class Student {
+        int sno;
+        int sname;
+        public Student (int a, int b) {
+            sno = a;
+            sname = b;
+        }
+        public string Print()
+        {
+            return sno+""+sname;
         }
     }
 }
